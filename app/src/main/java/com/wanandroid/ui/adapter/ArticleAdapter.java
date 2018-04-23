0617ad96e3fe9a706e05b2b878ab00885a4247c0
陈阳义 <674193357@qq.com>
2018-04-23 11:30:08 +0800

    优化文章列表样式

--- app/src/main/java/com/wanandroid/ui/adapter/ArticleAdapter.java
+++ app/src/main/java/com/wanandroid/ui/adapter/ArticleAdapter.java
@@ -5,6 +5,8 @@ import android.text.TextUtils;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
+import com.haozhang.lib.SlantedTextView;
+import com.ldoublem.thumbUplib.ThumbUpView;
 import com.wanandroid.bean.ArticleDatas;
 import com.wanandroid.chen.R;
 
@@ -26,7 +28,7 @@ public class ArticleAdapter extends BaseQuickAdapter<ArticleDatas, BaseViewHolde
         // 作者
         helper.setText(R.id.item_article_author, article.getAuthor());
         // 项目类型
-        helper.setText(R.id.item_article_project_type, article.getSuperchaptername() + " / " + article.getChaptername());
+        helper.setText(R.id.item_article_project_type, /*article.getSuperchaptername() + " / " + */article.getChaptername());
         // 时间
         helper.setText(R.id.item_article_time, article.getNicedate());
         // 标题
@@ -35,5 +37,21 @@ public class ArticleAdapter extends BaseQuickAdapter<ArticleDatas, BaseViewHolde
         // 内容
         helper.setText(R.id.item_article_content, Html.fromHtml(article.getDesc()));
         helper.setGone(R.id.item_article_content, TextUtils.isEmpty(article.getDesc()) ? false : true);
+        // 标签
+        SlantedTextView slantedTextView = helper.getView(R.id.item_article_stv);
+        slantedTextView.setText(article.getSuperchaptername());
+        // 收藏控件
+        ThumbUpView thumbUpView = helper.getView(R.id.item_article_collect);
+        if (article.getCollect()) {
+            thumbUpView.setLike();
+        } else {
+            thumbUpView.setUnlike();
+        }
+        thumbUpView.setOnThumbUp(new ThumbUpView.OnThumbUp() {
+            @Override
+            public void like(boolean like) {
+                article.setCollect(like);
+            }
+        });
     }
 }
