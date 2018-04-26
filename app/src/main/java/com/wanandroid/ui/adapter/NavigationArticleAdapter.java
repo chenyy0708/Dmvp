9321b08c09e076f72d43d4a7cd6fe4e3c9f61a17
陈阳义 <674193357@qq.com>
2018-04-26 09:53:33 +0800

    删除无用shape，使用SuperButton代替

--- app/src/main/java/com/wanandroid/ui/adapter/NavigationArticleAdapter.java
+++ app/src/main/java/com/wanandroid/ui/adapter/NavigationArticleAdapter.java
@@ -1,8 +1,10 @@
 package com.wanandroid.ui.adapter;
 
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.view.View;
 
+import com.allen.library.SuperButton;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 import com.wanandroid.bean.NavigationArticleBean;
@@ -18,13 +20,25 @@ import java.util.List;
  * @github :https://github.com/chenyy0708
  */
 public class NavigationArticleAdapter extends BaseQuickAdapter<NavigationArticleBean, BaseViewHolder> {
+
+    /**
+     * 颜色集合
+     */
+    private String[] mColors;
+
     public NavigationArticleAdapter(int layoutResId, @Nullable List<NavigationArticleBean> data) {
         super(layoutResId, data);
     }
 
     @Override
     protected void convert(BaseViewHolder helper, NavigationArticleBean item) {
-        helper.setText(R.id.tv_navigation_article_name, item.getTitle());
+        mColors = mContext.getResources().getStringArray(R.array.tab_colors);
+        SuperButton superButton = helper.getView(R.id.tv_navigation_article_name);
+        // 设置边框颜色
+        superButton.setText(item.getTitle());
+        int i = (int) (Math.random() * mColors.length);
+        String mColor = mColors[i];
+        superButton.setShapeStrokeColor(Color.parseColor(mColor)).setUseShape();
         helper.setOnClickListener(R.id.tv_navigation_article_name, new View.OnClickListener() {
             @Override
             public void onClick(View view) {
